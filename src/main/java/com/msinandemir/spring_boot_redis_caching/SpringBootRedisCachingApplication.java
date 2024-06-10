c5cf1a6da53ed6d256bef768c231ce4f30334597
Sinan <sinandeemir@gmail.com>
2024-06-11 00:59:53 +0300

    init commit

---
+++ src/main/java/com/msinandemir/spring_boot_redis_caching/SpringBootRedisCachingApplication.java
@@ -0,0 +1,13 @@
+package com.msinandemir.spring_boot_redis_caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRedisCachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msinandemir/spring_boot_redis_caching/SpringBootRedisCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msinandemir.spring_boot_redis_caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
