e2227f2d9bd21c48d40e093901b1d46bcdeccee8
Sinan <sinandeemir@gmail.com>
2024-06-11 01:05:33 +0300

    added: user model

---
+++ src/main/java/com/msinandemir/spring_boot_redis_caching/models/User.java
@@ -0,0 +1,32 @@
+package com.msinandemir.spring_boot_redis_caching.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @UpdateTimestamp
+    private Date updatedAt;
+
+    private String username;
+    private String password;
+}
